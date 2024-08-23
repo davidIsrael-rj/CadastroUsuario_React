d40ec10a90b2e8dadffc68e389cd5e1be58e7260
DavidIsrael <davidisrael.rj@gmail.com>
2024-08-23 18:59:02 -0300

    Componente Footer.jsx

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -3,5 +3,8 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido com <i className='fa fa-heart text-danger'></i> por 
+            <strong> David <span className='text-danger'>2024</span></strong>
+        </span>
     </footer>
